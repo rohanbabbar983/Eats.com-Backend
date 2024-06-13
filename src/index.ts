852d2594d9954cf9a75486e12371ccac2b5b2fc3
Rohan Babbar <rohanbabbar2003@gmail.com>
2024-06-14 03:08:56 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
-import restaurantRoute from "../src/routes/RestaurantRoute";
-import orderRoute from "./routes/OrderRoute"
+import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
+
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
@@ -18,6 +19,7 @@ cloudinary.config({
 });
 
 const app = express();
+
 app.use(cors());
 
 app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
@@ -29,13 +31,9 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant",myRestaurantRoute);
-app.use("/api/restaurant",restaurantRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 app.use("/api/order", orderRoute);
-
-
-
-
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
