112da18a1934c3e30972802cfbcb27fedcac0a52
Rohan Babbar <rohanbabbar2003@gmail.com>
2024-06-13 23:01:24 +0530

    Add files via upload

---
+++ src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,36 @@
+import express from "express"
+import multer from "multer";
+import MyRestaurantController from "../controller/MyRestaurantController"
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
+const router = express.Router();
+
+const storage= multer.memoryStorage();
+const upload = multer({
+    storage: storage,
+    limits:{
+        fileSize: 5*1024*1024, //5mb
+    }
+})
+
+router.get("/order",jwtCheck,jwtParse,MyRestaurantController.getMyrestaurantOrders);
+router.patch("/order/:orderId/status", jwtCheck,jwtParse,MyRestaurantController.updateOrderstatus);
+
+router.get("/", jwtCheck,jwtParse, MyRestaurantController.getMyrestaurant);
+router.post(
+    "/",
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    MyRestaurantController.createMyRestaurant
+);
+router.put(
+    "/", 
+    upload.single("imageFile"),
+    validateMyRestaurantRequest,
+    jwtCheck,
+    jwtParse,
+    MyRestaurantController.updateMyRestaurant
+);
+export default router;
---
+++ src/shared/types.ts
@@ -0,0 +1,10 @@
+export type UserType = {
+    auth0Id:string;
+    email:string;
+    password:string;
+    name:string;
+    addressLine1:string;
+    city:string;
+    country:string;
+
+}
